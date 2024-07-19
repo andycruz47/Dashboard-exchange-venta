80cc3f156525c1dd4d79fc0d4a357e29f5f4e837
Andy Cruz <andy10chicho@gmail.com>
2024-07-18 21:09:35 -0500

    quit comment

--- apis_net_pe.py
+++ apis_net_pe.py
@@ -34,7 +34,7 @@ class ApisNetPe:
             logging.warning(f"{response.url} - IP blocked")
         elif response.status_code == 429:
             time.sleep(int(response.headers["Retry-After"]))
-            #logging.warning(f"{response.url} - Many requests add delay")
+            logging.warning(f"{response.url} - Many requests add delay")
         elif response.status_code == 401:
             logging.warning(f"{response.url} - Invalid token or limited")
         else:
