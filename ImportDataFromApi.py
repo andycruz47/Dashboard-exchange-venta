5e88e0e8306b0a1a4c614dd2960657cc356278f4
Andy Cruz <andy10chicho@gmail.com>
2024-07-18 21:29:43 -0500

    Sleep reduced to 1s

--- ImportDataFromApi.py
+++ ImportDataFromApi.py
@@ -66,7 +66,7 @@ def getData(currentYear, currentMonth, lastMonth):
     # Convertir la lista de diccionarios en un DataFrame
     dfCurrentExchanges= pd.DataFrame(currentExchanges)
     # Sleep
-    time.sleep(3)
+    time.sleep(1)
     # Consulta a la API
     lastExchanges = getExchange(currentYear, lastMonth)
     # Convertir la lista de diccionarios en un DataFrame
